f0def02e64bcc15bdf342d56f7f09645b843b6a9
Oghuz Ali <113483494+SheikhOghuz@users.noreply.github.com>
2022-11-15 19:54:09 -0800

    Deleted stuff
    Didnt need em

--- 1to5.js
+++
@@ -1,8 +0,0 @@
-let newOutputDiv = document.getElementById("newOutput");
-let counter = 1,
-    target = 5;
-
-while (counter <= 5) {
-    newOutputDiv.innerHTML = newOutputDiv.innerHTML + counter;
-    counter = counter + 1; //counter==;
-}
