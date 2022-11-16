a9be089219c364e6a2b9327ced7dac0b066ee723
Oghuz Ali <113483494+SheikhOghuz@users.noreply.github.com>
2022-11-15 20:51:37 -0800

    Improving Decks
    Yes

--- pokemon.js
+++ pokemon.js
@@ -1,88 +1,84 @@
-fetch('pokemon.json')
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        displayPokemonCards(data)
+$(document).ready(function() {
+    $.get("https://uyghur.ai/course/data/pokemon.json", function(data, status) {
+        displayPokemonCards(data);
     });
 
-function displayPokemonCards(pokemonData) {
-    let pokemonDiv = document.getElementById("pokemonContainer");
-    let rows = pokemonData.length / 6;
-    let bgType = " ";
-    for (let eachRow = 0; eachRow < rows; eachRow++) {
-        let rowId = "row" + eachRow;
-        pokemonDiv.innerHTML += "<div class='row' id='" + rowId + "'></div>";
+    function displayPokemonCards(pokemonData) {
+        let pokemonDiv = document.getElementById("pokemonContainer");
+        let rows = pokemonData.length / 6;
+        let bgType = " ";
+        for (let eachRow = 0; eachRow < rows; eachRow++) {
+            let rowId = "row" + eachRow;
+            pokemonDiv.innerHTML += "<div class='row' id='" + rowId + "'></div>";
 
-        for (let i = 0; i < 6; i++) {
-            let currentIndex = eachRow * 6 + i;
-            if (pokemonData[currentIndex].type[0] == 'Water') {
-                bgType = "water-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Fire') {
-                bgType = "fire-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Grass') {
-                bgType = "grass-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Electric') {
-                bgType = "electric-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Bug') {
-                bgType = "bug-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Ghost') {
-                bgType = "ghost-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Normal') {
-                bgType = "normal-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Psychic') {
-                bgType = "psychic-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Dragon') {
-                bgType = "dragon-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Ice') {
-                bgType = "ice-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Poison') {
-                bgType = "poison-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Ground') {
-                bgType = "ground-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Rock') {
-                bgType = "rock-type";
-            } else if (pokemonData[currentIndex].type[0] == 'Fighting') {
-                bgType = "fighting-type";
-            } else {
-                bgType = " ";
-            }
-            if (pokemonData[currentIndex] != null || pokemonData[currentIndex] != undefined) {
-                document.getElementById(rowId).innerHTML +=
-                    "<div class='col-2'>" +
-                    "<div class='card h-100 " + bgType + " '>" +
-                    "<img src='" + pokemonData[currentIndex].img + "' onclick='pokemonSayHello(" + JSON.stringify(pokemonData[currentIndex]) + ")' class='card-img-top'>" +
-                    "<div class='card-body'>" +
-                    "<p class='card-text'></p></div>" +
-                    "<div class='card-footer card-text'>" + pokemonData[currentIndex].name + "</div>" +
-                    "</div></div>";
+            for (let i = 0; i < 6; i++) {
+                let currentIndex = eachRow * 6 + i;
+                if (pokemonData[currentIndex].type[0] == 'Water') {
+                    bgType = "water-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Fire') {
+                    bgType = "fire-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Grass') {
+                    bgType = "grass-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Electric') {
+                    bgType = "electric-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Bug') {
+                    bgType = "bug-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Ghost') {
+                    bgType = "ghost-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Normal') {
+                    bgType = "normal-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Psychic') {
+                    bgType = "psychic-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Dragon') {
+                    bgType = "dragon-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Ice') {
+                    bgType = "ice-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Poison') {
+                    bgType = "poison-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Ground') {
+                    bgType = "ground-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Rock') {
+                    bgType = "rock-type";
+                } else if (pokemonData[currentIndex].type[0] == 'Fighting') {
+                    bgType = "fighting-type";
+                } else {
+                    bgType = " ";
+                }
+                if (pokemonData[currentIndex] != null || pokemonData[currentIndex] != undefined) {
+                    document.getElementById(rowId).innerHTML +=
+                        "<div class='col-2'>" +
+                        "<div class='card h-100 " + bgType + " '>" +
+                        "<img src='" + pokemonData[currentIndex].img + "' onclick='pokemonSayHello(" + JSON.stringify(pokemonData[currentIndex]) + ")' class='card-img-top'>" +
+                        "<div class='card-body'>" +
+                        "<p class='card-text'></p></div>" +
+                        "<div class='card-footer card-text'>" + pokemonData[currentIndex].name + "</div>" +
+                        "</div></div>";
+                }
             }
         }
     }
-}
 
 
 
 
 
-function pokemonSayHello(pokemonData) {
-    document.getElementById("popup").innerHTML =
-        "<div> Name: " + pokemonData.name + "<hr> </div>" +
-        "<div> Type: " + pokemonData.type + "<hr> </div>" +
-        "<div> ID: " + pokemonData.num + "<hr> </div>" +
-        "<div> Weaknesses: " + pokemonData.weaknesses + "<hr> </div>" +
-        "<div> Height: " + pokemonData.height + "<hr> </div>" +
-        "<div> Weight: " + pokemonData.weight + "<hr> </div>" +
-        "<div> <img src='" + pokemonData.img + "'><hr></div>" +
-        "<button onclick ='closebtn()'>Close Popup</button>";
-}
+    function pokemonSayHello(pokemonData) {
+        document.getElementById("popup").innerHTML =
+            "<div> Name: " + pokemonData.name + "<hr> </div>" +
+            "<div> Type: " + pokemonData.type + "<hr> </div>" +
+            "<div> ID: " + pokemonData.num + "<hr> </div>" +
+            "<div> Weaknesses: " + pokemonData.weaknesses + "<hr> </div>" +
+            "<div> Height: " + pokemonData.height + "<hr> </div>" +
+            "<div> Weight: " + pokemonData.weight + "<hr> </div>" +
+            "<div> <img src='" + pokemonData.img + "'><hr></div>" +
+            "<button onclick ='closebtn()'>Close Popup</button>";
+    }
 
-function closebtn() {
-    document.querySelector('.popup').style.display = 'none';
-}
+    function closebtn() {
+        document.querySelector('.popup').style.display = 'none';
+    }
 
 
-$(document).ready(function() {
     $("#hide-type").click(function() {
         let selectedType = $('#pokemonTypeOption :selected').text();
         if (selectedType == "Water") {
